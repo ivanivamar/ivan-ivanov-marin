d83422bd839a1e13898e85c78a9d783994dbdfb3
ivanivamar <ivanivamar99@gmail.com>
2024-02-01 14:33:25 +0100

    navigation and main heights and home init

---
+++ src/components/top-nav.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav>
+        <div class="left-nav">
+            <a href="/" style="width: 300px; text-align: left">ivan-ivanov-marin</a>
+
+            <div class="hug flex align-items-center">
+                <RouterLink to="/">_hello</RouterLink>
+                <RouterLink to="/about">_about-me</RouterLink>
+                <RouterLink to="/projects">_projects</RouterLink>
+            </div>
+        </div>
+
+        <RouterLink to="/contact" style="border-left: 1px solid var(--lines)">_contact-me</RouterLink>
+	</nav>
+</template>
+
+<style scoped>
+
+</style>
+<script setup lang="ts">
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="home-gradient gradient-green"></div>
+    <div class="home-gradient gradient-blue"></div>
 </template>
+
+<style scoped>
+    .home-gradient {
+        position: absolute;
+        filter: blur(87px);
+        opacity: 0.4;
+        width: 424px;
+        height: 322px;
+    }
+    .gradient-green {
+        right: 24%;
+        top: 20%;
+
+        background: #43D9AD;
+        transform: rotate(-94.3deg);
+    }
+    .gradient-blue {
+        right: 14%;
+        top: 48%;
+
+        background: #4D5BCE;
+        transform: rotate(13.51deg);
+    }
+</style>
