dce5366ec37d48b7fd02cb1f71c30d58a83088f4
Ivan Ivanov <ivanivamar99@gmail.com>
2026-01-29 16:03:12 +0100

    Adds work experience component with about tree
    Introduces a dynamic tree structure for the about section.
    
    This structure allows users to navigate and explore different sections such as work experience, skills, and education.
    The tree component is reusable and configurable, making it easy to add or modify content.
    
    Removes the section wrapper from the about page, streamlining the layout.
    Updates the menu icon to a rounded version for visual consistency.
    Renames 'about-tree.sass' to 'cards.sass' reflecting its broader usage.

--- src/app/about/work-experience/work-experience.ts
+++ src/app/about/work-experience/work-experience.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { AboutTree, IAboutTree } from '../../../layout/about-tree/about-tree'
 
 @Component({
   selector: 'app-work-experience',
-  imports: [],
+    imports: [
+        AboutTree
+    ],
   templateUrl: './work-experience.html',
   styleUrl: './work-experience.sass',
 })
 export class WorkExperience {
-
+    options: IAboutTree[] = [
+        {
+            title: 'Work Experience',
+            extended: true,
+            children: [
+                {
+                    title: 'Angular Developer',
+                },
+                {
+                    title: 'Salesforce B2C Developer',
+                }
+            ]
+        },
+        {
+            title: 'Skills',
+            children: [
+                {
+                    title: 'Angular',
+                },
+                {
+                    title: 'C#',
+                }
+            ]
+        },
+        {
+            title: 'Education',
+            children: [
+                {
+                    title: '',
+                },
+                {
+                    title: '',
+                }
+            ]
+        }
+    ]
 }
