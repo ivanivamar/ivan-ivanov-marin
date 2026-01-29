1b99ba5cb7323de9b817dca9b9873e090aaafd03
Ivan Ivanov <ivanivamar99@gmail.com>
2026-01-29 16:44:57 +0100

    Adds project portfolio section
    Adds a new section to showcase projects with titles, images, and descriptions.
    
    Configures the build process to include assets and updates styling
    for improved visual presentation.

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -2,10 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+    projects: IProject[] = [
+        {
+            title: '_caae',
+            image: 'assets/caae.png',
+            description: 'Migration of a Windows Forms application to a modern Angular web application.'
+        },
+        {
+            title: '_provalliance',
+            image: 'assets/provalliance.png',
+            description: 'Scheduler Angular application for managing appointments and resources.'
+        },
+        {
+            title: '_senner-navigation-app',
+            image: 'assets/senner.png',
+            description: 'Navigation app built with MAUI for logistics and transportation management.'
+        },
+    ];
+}
 
+export interface IProject {
+    title: string;
+    image: string;
+    description: string;
 }
