df29d6a5d8fb9a0424ee5ad8d8d8930cb885b805
Ivan Ivanov <ivanivamar99@gmail.com>
2026-03-19 17:20:03 +0100

    feat: add skills section

---
+++ src/app/landing-page/skills/skills.ts
@@ -0,0 +1,37 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {SectionAnimationDirective} from "../../../shared/directives/section-animation.directive";
+import {TranslatePipe} from '@ngx-translate/core';
+
+@Component({
+    selector: 'app-skills',
+    imports: [
+        SectionAnimationDirective,
+        TranslatePipe
+    ],
+    templateUrl: './skills.html',
+    styleUrl: './skills.sass',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Skills {
+    frontendSkills = [
+        'Angular',
+        'TypeScript',
+        'RxJS',
+        'Jest/Jasmine',
+        'GSAP',
+        'PrimeNG',
+    ];
+
+    backendSkills = [
+        'C#',
+        'SQL',
+        'PostgreSQL',
+        'TypeScript'
+    ];
+
+    toolsSkills = [
+        'Webstorm',
+        'Git',
+        'Figma'
+    ]
+}
