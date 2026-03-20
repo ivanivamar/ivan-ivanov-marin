a01689b1a8f020cb6d35a2aeff15988e20d6cdd5
Ivan Ivanov <ivanivamar99@gmail.com>
2026-03-20 14:35:43 +0100

    feat: implement dynamic work details page with project-specific data and responsive styles

--- src/app/works/work-details/work-details.ts
+++ src/app/works/work-details/work-details.ts
@@ -1,15 +1,28 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {SectionAnimationDirective} from '../../../shared/directives/section-animation.directive';
+import {Project, projects} from '../works';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
+import {NgClass} from '@angular/common';
 
 @Component({
     selector: 'app-work-details',
     imports: [
-        SectionAnimationDirective
+        SectionAnimationDirective,
+        RouterLink,
+        TranslatePipe,
+        NgClass
     ],
     templateUrl: './work-details.html',
     styleUrl: './work-details.sass',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WorkDetails {
+    private route = inject(ActivatedRoute);
+    // get the current project from the url with the title
+    protected readonly project = projects().find(p => p.title === this.route.snapshot.paramMap.get('title'));
 
+    constructor() {
+        console.log(this.route.snapshot);
+    }
 }
